238672620e7c715d479c37eafe9bf51a440d866a
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-05-04 20:40:47 +0300

    create classes for run application and run services

---
+++ ScheduleClassBot/ScheduleClassBot/Internal/Run.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using NLog;
+using ScheduleClassBot.Internal;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+
+namespace ScheduleClassBot;
+internal class Run : IHostedService
+{
+    private readonly IConfiguration _cfg;
+
+    public Run(IConfiguration cfg)
+    {
+        _cfg = cfg;
+    }
+
+    private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        try
+        {
+            var token = _cfg.GetSection("Tokens")["TelegramBotToken"];
+            var telegramBot = new TelegramBotClient(token!);
+
+            _logger.Info($"Бот {telegramBot.GetMeAsync(cancellationToken: cancellationToken).Result.FirstName} успешно запущен!");
+            var cts = new CancellationTokenSource();
+            cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { },
+            };
+
+            telegramBot.StartReceiving(
+                new DefaultUpdateHandler(ProcessingMessage.HandleUpdateAsync, ProcessingMessage.HandleErrorAsync),
+                receiverOptions,
+                cancellationToken
+            );
+        }
+        catch (Exception ex)
+        {
+            _logger.Error($"Error message: {ex.Message}");
+        }
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken = default)
+    {
+        _logger.Info("Service stopping");
+        return Task.CompletedTask;
+    }
+
+}
---
+++ ScheduleClassBot/ScheduleClassBot/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NLog;
+using ScheduleClassBot;
+
+namespace TelegramBotExperiments;
+
+class Program
+{
+    private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            using IHost host = Host.CreateDefaultBuilder()
+                .ConfigureHostConfiguration(cfgBuilder => { cfgBuilder.AddJsonFile("appsettings.json"); })
+                .ConfigureServices(
+                    services => { services.AddHostedService<Run>(); })
+                .UseWindowsService(options => { options.ServiceName = ".NET StockBot"; })
+                .Build();
+
+            await host.RunAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex, "Error running app");
+        }
+    }
+}

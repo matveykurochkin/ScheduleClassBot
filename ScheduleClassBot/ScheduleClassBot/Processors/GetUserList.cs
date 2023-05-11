6c803c0ad0c896e5aa99cd3df7be078848817d31
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-05-11 15:53:53 +0300

    create method for view list users

---
+++ ScheduleClassBot/ScheduleClassBot/Processors/GetUserList.cs
@@ -0,0 +1,43 @@
+﻿using NLog;
+using System.Text;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ScheduleClassBot.Processors;
+internal class GetUserList
+{
+    private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+    private static string projectPath = AppDomain.CurrentDomain.BaseDirectory;
+
+    public static async Task GetUsersList(ITelegramBotClient botClient, Update update, Message message, CancellationToken cancellationToken)
+    {
+        try
+        {
+            string fileContent = "", path = Path.Combine(projectPath, "ListUsers.txt");
+
+            if (System.IO.File.Exists(path))
+            {
+                fileContent = System.IO.File.ReadAllText(path);
+                StringBuilder responseBuilder = new StringBuilder();
+
+                foreach (string line in fileContent.Split('\n'))
+                {
+                    if (!string.IsNullOrWhiteSpace(line))
+                        responseBuilder.AppendLine($"User Info: {line.Trim()}");
+                }
+
+                await botClient.SendTextMessageAsync(message.Chat, $"{update.Message?.From?.FirstName}, держи список пользователей:\n{fileContent}", cancellationToken: cancellationToken);
+                _logger.Info($"SPECIAL COMMAND!!! View users list success!");
+            }
+            else
+            {
+                await botClient.SendTextMessageAsync(message.Chat, $"{update.Message?.From?.FirstName}, пользователей нет!", cancellationToken: cancellationToken);
+                _logger.Info($"SPECIAL COMMAND!!! Error view users list success!");
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.Error($"SPECIAL COMMAND!!! Error view users list. Error message: {ex.Message}");
+        }
+    }
+}

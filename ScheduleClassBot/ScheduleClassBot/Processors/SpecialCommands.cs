99c049d0e55e498daf0f86037e6c502d0695fe62
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-05-15 18:59:05 +0300

    add new special command "get log file"

--- ScheduleClassBot/ScheduleClassBot/Processors/SpecialCommands.cs
+++ ScheduleClassBot/ScheduleClassBot/Processors/SpecialCommands.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace ScheduleClassBot.Processors;
 internal class SpecialCommands
@@ -9,6 +10,7 @@ internal class SpecialCommands
     private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
     private static string projectPath = AppDomain.CurrentDomain.BaseDirectory;
     internal static long countMessage = 0;
+    private static string pathOnProject = "", path = "", logdate = "";
     public static async Task GetUsersList(ITelegramBotClient botClient, Update update, Message message, CancellationToken cancellationToken)
     {
         try
@@ -52,6 +54,39 @@ internal class SpecialCommands
         catch (Exception ex)
         {
             _logger.Error($"!!!SPECIAL COMMAND!!! Error view count message. Error message: {ex.Message}");
+        }
+    }
+
+    public static async Task GetLogFile(ITelegramBotClient botClient, Update update, Message message, CancellationToken cancellationToken)
+    {
+        try
+        {
+            if (!(message.Text! == "specialcommandforgetlogfile"))
+            {
+                int index = message.Text!.IndexOf(":");
+                if (index != -1)
+                    logdate = message.Text!.Substring(index + 1).Trim();
+
+                pathOnProject = AppDomain.CurrentDomain.BaseDirectory;
+                path = Path.Combine(pathOnProject, $"log/{logdate}");
+
+                if (System.IO.File.Exists(path))
+                {
+                    using (FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                    {
+                        InputFileStream inputFile = new InputFileStream(fileStream, logdate);
+                        await botClient.SendDocumentAsync(message.Chat, inputFile, caption: $"{update.Message?.From?.FirstName}, держи логи за выбранный день!");
+                    }
+                }
+                else
+                    await botClient.SendTextMessageAsync(message.Chat, $"{update.Message?.From?.FirstName}, данного файла не обнаружено! Проверь корректность введенной даты!", cancellationToken: cancellationToken);
+            }
+            else
+                await botClient.SendTextMessageAsync(message.Chat, $"{update.Message?.From?.FirstName}, команда выглядит следующим образом:\n```\nspecialcommandforgetlogfile:yyyy-mm-dd.log\n```", parseMode: ParseMode.Markdown, cancellationToken: cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            _logger.Error($"!!!SPECIAL COMMAND!!! Error get log file. Error message: {ex.Message}");
         }
     }
 }

3ba871c38671b9370b64bf36946a1f2fb49c4519
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-05-22 20:58:08 +0300

    create special buttons

---
+++ ScheduleClassBot/ScheduleClassBot/Internal/SpecialBotButton.cs
@@ -0,0 +1,34 @@
+﻿using Telegram.Bot.Types.ReplyMarkups;
+
+namespace ScheduleClassBot.Internal;
+internal class SpecialBotButton
+{
+    public static IReplyMarkup SpecialCommandButton()
+    {
+        var tgButton = new ReplyKeyboardMarkup(new[]
+        {
+    new[]
+    {
+        new KeyboardButton("specialcommandforviewlistusers")
+    },
+        new[]
+    {
+        new KeyboardButton("specialcommandforviewcountmessages")
+    },
+        new[]
+    {
+        new KeyboardButton("specialcommandforgetlogfile")
+    },
+        new[]
+    {
+        new KeyboardButton("specialcommandforcheckyourprofile")
+    },
+        new[]
+    {
+        new KeyboardButton("Назад ⬅")
+    }
+     });
+        tgButton.ResizeKeyboard = true;
+        return tgButton;
+    }
+}

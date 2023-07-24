2ec84b356b261221bccdbf9b4fe690893d3c4e5d
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-07-24 15:14:50 +0300

    create new class that represents appsettings.json

---
+++ ScheduleClassBot/Configuration/BotSettingsConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace ScheduleClassBot.Configuration;
+
+public class BotSettingsConfiguration
+{
+    public BotTokenSection? BotToken { get; set; }
+    public UserIdSection? UserId { get; set; }
+    public OpenAiSection? OpenAi { get; set; }
+}
+
+public class BotTokenSection
+{
+    public string? TelegramBotToken { get; set; }
+}
+
+public class UserIdSection
+{
+    public List<long>? IdUser { get; set; }
+}
+
+public class OpenAiSection
+{
+    public string? ChatGptKey { get; set; }
+}

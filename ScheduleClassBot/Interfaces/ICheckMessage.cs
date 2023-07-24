e519b936104c2ef0717316eb75afbc12b7e9c7ef
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-07-24 18:35:39 +0300

    create interface for method CheckingMessageText

---
+++ ScheduleClassBot/Interfaces/ICheckMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace ScheduleClassBot.Interfaces;
+
+internal interface ICheckMessage
+{
+    /// <summary>
+    /// Используется для сравнения двух строк на равенство.
+    /// Метод возвращает значение true, если строки совпадают, и
+    /// false, если строки не совпадают.
+    /// </summary>
+    /// <param name="receivedText">полученный на вход текст</param>
+    /// <param name="necessaryText">текст с которым надо сравнить</param>
+    /// <returns></returns>
+    public bool CheckingMessageText(string receivedText, string necessaryText);
+}
